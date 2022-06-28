f35209e9364c97f5ae64b05249c5a8c48989bcaa
Fredrick W. Warren <fredw@northriverboats.com>
2022-06-28 15:16:29 +0000

    skelleton for petite vue-js app
    - petite vue loaded and activated on page
    - google fonts loaded
    - themed with tailwind and daisy-ui

---
+++ app/Views/app.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+    <title>North River Boats Open File Report</title>
+    <!-- javascript -->
+    <script src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"></script>
+
+    <!-- fonts -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=B612+Mono:wght@400;700&family=Roboto&display=swap" rel="stylesheet" />
+
+    <!-- tailwind / daisyui -->
+    <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.1/dist/tailwind.min.css" rel="stylesheet" type="text/css" />
+    <link href="https://cdn.jsdelivr.net/npm/daisyui@1.10.0/dist/full.css" rel="stylesheet" type="text/css" />
+    <link href="css/app.css" rel="stylesheet" type="text/css" />
+  </head>
+  <body style="">
+    <div v-scope="{ count: 0 }">
+      {{ count }}
+      <button @click="count++">inc</button>
+    </div>
+    <script type="module" src="js/app.js"></script>
+  </body>
+</html>
---
+++ public/js/app.js
@@ -0,0 +1,5 @@
+/* petite vue application */
+import { createApp } from 'https://unpkg.com/petite-vue@0.2.3/dist/petite-vue.es.js' 
+
+createApp({
+}).mount()
